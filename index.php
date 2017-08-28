41e4023667c4c28371b28f8bd1d2771b4c8c3df3
Jamie Lam <jamielam@Jamies-MacBook-Pro.local>
2017-08-27 21:47:40 -0600

    initial push

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+</head>
+<body>
+    <h3>My First PHP program</h3>
+
+    <?php
+    echo "hello world!";
+    ?>
+
+    <!-- <a href = "secondPage.php">CLICK ME</a> -->
+
+</body>
+</html>
