2601abf5d142c5a2394178c1dfa4004257344dfa
Jamie Lam <jamielam@Jamies-MacBook-Pro.local>
2017-09-07 21:34:21 -0600

    added while loop

--- CE02.php
+++ CE02.php
@@ -59,7 +59,16 @@
           $a +=10;
           $b +=5;
       }
-      echo ("At the end of the loop a = $a and b = $b");
+      echo ("At the end of the loop a = $a and b = $b<br>");
+
+      $i = 0;
+      $num = 50;
+
+      while($i <10) {
+          $num--;
+          $i++;
+      }
+      echo ("Loop stopped at i = $i and num = $num<br>");
     ?>
 
 </body>
