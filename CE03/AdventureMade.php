a03fe8f69d7bbc43581f0cc0e35d0e0cd6211398
Jamie Lam <jamielam@Jamies-MacBook-Pro.local>
2017-09-20 13:05:17 -0600

    removed >

--- CE03/AdventureMade.php
+++ CE03/AdventureMade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content = "width=device-width, initial-scale=1">
 </head>
 
-<<style>
+<style>
     img {
             behavior:url(css/iepngfix.htc);
             border:none;
