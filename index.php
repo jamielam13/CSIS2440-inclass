9ad2833771f050b73ca7efb14c61618c3108ea00
Jamie Lam <jamielam@Jamies-MacBook-Pro.local>
2017-09-20 19:28:01 -0600

    added break

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <h2>Inclass work</h2>
     <a href="CE01.php">CE01</a> <br>
     <a href="CE02.php">CE02</a> <br>
-    <a href="/CE03/CE03.php">CE03</a>
+    <a href="/CE03/CE03.php">CE03</a><<br>
     <a href="/CE04/CE04.php">CE04</a>
 
 </body>
