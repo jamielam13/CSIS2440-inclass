6e8e3564ab3bf0e33c456b28802255e8d1bc5054
Jamie Lam <jamielam@Jamies-MacBook-Pro.local>
2017-09-25 18:40:05 -0600

    updated echo

--- CE04/PHPsource.php
+++ CE04/PHPsource.php
@@ -1,21 +1,22 @@
 <?php
     //showing form if it is not filled out
     if ($_POST['hidden1'] == 0) {
-        print <<<END
-        Please Create a Character to put into the story.<br>
-        Name: <input type="text" name="Name"><br>
-    Age: <input type="number" name="Age"><br>
-    Gender: F<input type="radio" value="F" name="Gender" selected="true">  M<input type="radio" value="M" name="Gender"><br>
-    Class: <select name="Class">
-        <option value="Detective">Detective</option>
-        <option value="Scientest">Scientest</option>
-        <option value="Soldier">Soldier</option>
-        <option value="Doctor">Doctor</option>
-    </select><br>
-    <input type="submit" value="Show Me" name="Create"><br>
+        echo("here")
+//         print <<<END
+//         Please Create a Character to put into the story.<br>
+//         Name: <input type="text" name="Name"><br>
+//     Age: <input type="number" name="Age"><br>
+//     Gender: F<input type="radio" value="F" name="Gender" selected="true">  M<input type="radio" value="M" name="Gender"><br>
+//     Class: <select name="Class">
+//         <option value="Detective">Detective</option>
+//         <option value="Scientest">Scientest</option>
+//         <option value="Soldier">Soldier</option>
+//         <option value="Doctor">Doctor</option>
+//     </select><br>
+//     <input type="submit" value="Show Me" name="Create"><br>
         
-        <input type="hidden" value ="1" name="hidden1">
-END;
+//         <input type="hidden" value ="1" name="hidden1">
+// END;
     } else {
         $name = ucwords(strtolower($_POST['Name']));
         // $name = $_POST['Name'];
@@ -45,10 +46,14 @@ END;
                 . "is a $class<br>"
                 . "This is the start of the story....<br>");
 
-        echo $settings[0] . ' ' . $objectives[0] 
-            . ' ' . $antagonists[0] . ' '. $complications[0] 
-            . "<br><input type='submit' value='Try Again' "
-        . "name='Create'><br><input type='hidden' "
-                . "value ='0'name='hidden1'>";
+        // echo $settings[0] . ' ' . $objectives[0] 
+        //     . ' ' . $antagonists[0] . ' '. $complications[0] 
+        //     . "<br><input type='submit' value='Try Again' "
+        // . "name='Create'><br><input type='hidden' "
+        //         . "value ='0'name='hidden1'>";
+
+        echo $settings[0]." ".$objectives[0]." ".$antagonists[0]." "<div class="$complications"
+            .<br><input type="submit" value "Try Again" name="Create"><br><input type="hidden"
+            value="0" name="hidden1">
     }    
 ?>
